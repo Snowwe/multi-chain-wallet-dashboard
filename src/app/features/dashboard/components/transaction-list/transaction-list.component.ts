2bf487d97309e4136b9ec2aff45e1caca0f081f1
Snezhana Novitskaya <snezhana@codiq.io>
2026-03-15 22:09:35 +0300

    feat: add transaction list component

--- src/app/features/dashboard/components/transaction-list/transaction-list.component.ts
+++ src/app/features/dashboard/components/transaction-list/transaction-list.component.ts
@@ -1,13 +1,27 @@
-import { Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { CommonModule, DatePipe } from '@angular/common';
 
 import { TransactionItem } from '@models/transaction.model';
+import { formatTokenBalance, shortenMiddle } from '@utils/balance.util';
 
 @Component({
   selector: 'app-transaction-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, DatePipe],
   templateUrl: './transaction-list.component.html',
   styleUrl: './transaction-list.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TransactionListComponent {
   readonly transactions = input<TransactionItem[]>([]);
+
+  shorten(value: string): string {
+    return shortenMiddle(value);
+  }
+
+  formatAmount(transaction: TransactionItem): string {
+    return `${formatTokenBalance(transaction.rawBalance, transaction.decimals)} ${
+      transaction.symbol
+    }`;
+  }
 }
